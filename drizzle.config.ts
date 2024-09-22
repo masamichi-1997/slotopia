7257af1a7221cf96ce4a575f0768221493f31de0
masamichi tozawa <fortube7716@gmail.com>
2024-09-22 10:58:51 +0900

    ⚙ 初期設定変更: 設定をプロジェクトに合わせて調整

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,19 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv'
+import { defineConfig } from 'drizzle-kit'
 
-config({ path: '.env' });
+config({ path: '.env' })
 
 export default defineConfig({
   schema: './db/table/*',
   out: './supabase/migrations',
   dialect: 'postgresql',
+  strict: true,
+  breakpoints: true,
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    database: process.env.DATABASE!,
+    port: 5432,
+    host: process.env.DATABASE_HOST!,
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
   },
-});
+})
